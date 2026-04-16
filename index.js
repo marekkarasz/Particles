c450d57ec3abcc4cc59f56def1f5eb380e648b55
Marek Karasz <marekkarasz@googlemail.com>
2026-04-16 17:38:53 +0100

    adding the gradient and configuring colors for particles

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ console.log(ctx);
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.x = Math.random() * this.effect.width;
-    this.y = Math.random() * this.effect.height;
-    this.radius = 20;
+    this.radius = 15;
+    this.x = this.radius + Math.random() * this.effect.width;
+    this.y = this.radius + Math.random() * this.effect.height;
   }
   draw(context) {
+    context.fillStyle = `hsl(${this.x * 0.08}, 100%, 50%)`;
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
   }
 }
 
@@ -26,7 +28,7 @@ class Effect {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.particles = [];
-    this.numberOfParticles = 20;
+    this.numberOfParticles = 200;
     this.createParticles();
   }
   createParticles() {
